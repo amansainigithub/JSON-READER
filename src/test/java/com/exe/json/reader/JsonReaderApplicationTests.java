26e7980edb964a1dc9eefea0617219b9c331e0fc
aman <amansaini1407@gmail.com>
2023-07-20 00:26:27 +0530

    https://github.com/amansainigithub/JSON_READER.git

---
+++ src/test/java/com/exe/json/reader/JsonReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.json.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
